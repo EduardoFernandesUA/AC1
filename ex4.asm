08a26a12b2eb482f64cbacabc7062198dff3688e
Your Name <edu.fernandes@ua.pt>
2021-12-01 13:56:50 +0000

    ex4

---
+++ ex4.asm
@@ -0,0 +1,12 @@
+# Mapa de Registos
+#  i
+#  
+	.data
+	.text
+	.eqv	print_int10,1
+	.eqv	print_string,4
+str1:	.asciiz	"Nr. de parametros: "
+str2:	.asciiz	"\nP"
+str3:	.asciiz	": "
+	.globl main
+main:
